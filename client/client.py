c7cb2ebfa1d75773e009e55f0b62eae186d4f537
Boris Starkov <boris.starkow@gmail.com>
2021-06-13 20:53:35 +0300

    basic rooms functionality

--- client/client.py
+++ client/client.py
@@ -1,15 +1,51 @@
 import asyncio
+import json
+
 import websockets
 
-async def hello():
+state = 'lobby'
+
+async def selectRoom(websocket):
+    global state
+
+    while True:
+        print('Доступные комнаты:')
+        print('-' * 16)
+        await websocket.send(json.dumps({'type': 'request', 'subject': 'rooms'}))
+        rooms = json.loads(await websocket.recv())['content']
+        for i, room in enumerate(rooms):
+            print(f'Комната #{i}: {room["players"]} игроков, {"открыта" if room["state"] == "open" else "закрыта"}')
+        print('-' * 16)
+        while True:
+            resp = input('Введите номер комнаты, чтобы присоединиться к ней. Либо "new", чтобы создать новую.')
+            if resp == 'new':
+                await websocket.send(json.dumps({'type': 'update', 'subject': 'rooms'}))
+                break
+            elif resp.isnumeric() and int(resp) < len(rooms):
+                await websocket.send(json.dumps({'type': 'update', 'subject': 'connect_to_room', 'content': resp}))
+                res = json.loads(await websocket.recv())
+                if res['code'] == 'error':
+                    print(f'Ошибка при входе в комнату: {res["content"]}')
+                else:
+                    state = 'room'
+                    break
+            else:
+                print(f'Такой комнаты нет: {resp}, попробуйте снова')
+                continue
+        if state != 'lobby':
+            break
+
+async def handler():
     uri = "ws://localhost:8765"
+    global state
+
     async with websockets.connect(uri) as websocket:
-        name = input("What's your name? ")
+        while True:
+            if state == 'lobby':
+                room = await selectRoom(websocket)
+            elif state == 'room':
+                pass
+            else:
+                raise Exception('unknown state')
 
-        await websocket.send(name)
-        print(f"> {name}")
-
-        greeting = await websocket.recv()
-        print(f"< {greeting}")
-
-asyncio.get_event_loop().run_until_complete(hello())
+asyncio.get_event_loop().run_until_complete(handler())
--- server/server.py
+++ server/server.py
@@ -1,16 +1,98 @@
 import asyncio
 import websockets
+import logging
+import json
 
-async def hello(websocket, path):
-    name = await websocket.recv()
-    print(f"< {name}")
+logging.basicConfig()
 
-    greeting = f"Hello {name}!"
+CONNECTIONS = set()
 
-    await websocket.send(greeting)
-    print(f"> {greeting}")
+global_state = {
+    'rooms': []
+}
 
-start_server = websockets.serve(hello, "localhost", 8765)
+local_state = dict()
+
+
+class Room:
+    def __init__(self):
+        self.state = 'open'
+        self.connections = []
+        self.readycnt = 0
+
+    def close(self):
+        self.state = 'closed'
+
+    def subscribe(self, ws):
+        self.connections.append(ws)
+
+    def unsubscribe(self, ws):
+        self.connections.remove(ws)
+
+async def register_connection(websocket):
+    CONNECTIONS.add(websocket)
+    global local_state
+    local_state[websocket] = {
+        'room': None
+    }
+
+
+async def unregister_connection(websocket):
+    CONNECTIONS.remove(websocket)
+    global global_state
+    global local_state
+    if 'room' in local_state[websocket]:
+        rid = local_state[websocket]['room']
+        global_state['rooms'][rid].unsubscribe(websocket)
+    local_state.pop(websocket)
+
+
+async def multicast(message):
+    if CONNECTIONS:
+        await asyncio.wait([connection.send(message) for connection in CONNECTIONS])
+    print(f'multicast > {message}')
+
+
+async def consumer(message, ws):
+    global global_state
+
+    if message['type'] == 'request':
+        if message['subject'] == 'rooms':
+            await ws.send(json.dumps({
+                'type': 'response',
+                'code': 'ok',
+                'content': list(map(lambda room: {'state': room.state, 'players': len(room.connections)}, global_state['rooms']))
+            }))
+        else:
+            logging.error(f'unsupported subject: {subject}')
+    elif message['type'] == 'update':
+        if message['subject'] == 'rooms':
+            global_state['rooms'].append(Room())
+        elif message['subject'] == 'connect_to_room':
+            id = message['content']
+            if not id.isnumeric() or int(id) >= len(global_state['rooms']):
+                await ws.send(json.dumps({'type': 'response', 'code': 'error', 'content': f'{id} is not a room'}))
+            elif global_state['rooms'][int(id)].state == 'closed':
+                await ws.send(json.dumps({'type': 'response', 'code': 'error', 'content': f'room {id} is closed'}))
+            else:
+                global_state['rooms'][int(id)].subscribe(ws)
+                local_state[ws]['room'] = int(id)
+                await ws.send(json.dumps({'type': 'response', 'code': 'ok'}))
+        else:
+            logging.error(f'unsupported subject: {subject}')
+    else:
+        logging.error(f'unsupported subject: {subject}')
+
+
+async def handler(websocket, path):
+    await register_connection(websocket)
+    try:
+        async for message in websocket:
+            await consumer(json.loads(message), websocket)
+    finally:
+        await unregister_connection(websocket)
+
+start_server = websockets.serve(handler, "localhost", 8765)
 
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
